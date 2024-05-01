fe2a314fe2143313e708726450903feca2ae4208
Aidan <43829909+Ouckah@users.noreply.github.com>
2024-05-01 12:38:50 -0400

    Create update_readmes.py
    UGHHGHUGUHGUHG

---
+++ .github/scripts/update_readmes.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import util
+
+
+def main():
+
+    listings = util.getListingsFromJSON()
+
+    util.checkSchema(listings)
+    util.sortListings(listings)
+
+    summer_2024_listings = util.filterSummer(listings)
+    util.embedTable(summer_2024_listings, "README.md")
+
+    util.setOutput("commit_message", "Updating READMEs at " + datetime.now().strftime("%B %d, %Y %H:%M:%S"))
+
+
+if __name__ == "__main__":
+    main()
