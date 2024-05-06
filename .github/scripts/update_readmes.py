c53cd1bf948fb9b35cbb027bc63d42918ea27852
Aidan <43829909+Ouckah@users.noreply.github.com>
2024-05-05 20:31:03 -0400

    Update update_readmes.py

--- .github/scripts/update_readmes.py
+++ .github/scripts/update_readmes.py
@@ -7,7 +7,7 @@ def main():
     listings = util.getListingsFromJSON()
 
     util.checkSchema(listings)
-    util.sortListings(listings)
+    listings = util.sortListings(listings) # no longer in place my bad :(
     util.embedTable(listings, "README.md")
 
     util.setOutput("commit_message", "Updating READMEs at " + datetime.now().strftime("%B %d, %Y %H:%M:%S"))
